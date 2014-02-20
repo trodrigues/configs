09db9a467091ce826fc2a435a30a9bd32fdd9df2
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-02-20 14:41:03 +0100

    contentful helpers

--- zsh/contentful.zsh
+++ zsh/contentful.zsh
@@ -1,2 +1,30 @@
 export CONTENTFUL_ACCESS_TOKEN="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 export CF_EMAIL="tiago@contentful.com"
+
+cdtocontentfului(){
+  if [[ "`pwd`" =~ \/contentful\/lab\/user_interface ]] ; then
+    cd $HOME/contentful/lab/user_interface/$1
+  fi
+}
+
+cdui(){
+  cdtocontentfului
+}
+
+cdjs(){
+  cdtocontentfului 'app/assets/javascripts'
+}
+
+cdcss(){
+  cdtocontentfului 'app/assets/stylesheets'
+}
+
+cdunit(){
+  cdtocontentfului 'spec/javascripts/contentful/unit'
+}
+
+cdspecjs(){
+  cdtocontentfului 'spec/javascripts'
+}
+
+
