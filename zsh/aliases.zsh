4635f388517654af60ed4f0adf7c91482b599c33
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-01-24 18:33:20 +0100

    aliases cleanup and new docker aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,31 +9,38 @@ alias mdkir="mkdir -p"
 
 alias timestamp="date \"+%Y%m%d%H%M%S\""
 
-alias rm_pyc="find . -name \"*.pyc\" -exec rm '{}' \;"
-alias rm_swp="find . -name \"*.swp\" -exec rm '{}' \;"
 alias rm_vim_mchars="perl -pi -e '$m=chr(13);s/$m//g'"
-alias killpid="$HOME/code/bashscripts/kill_by_pid.sh"
 
-alias tailaccess="tail -f $httpd_accesslog"
-alias tailerror="tail -f $httpd_errorlog"
 alias reloadbashrc="cp $HOME/Dropbox/configs/bashrc $HOME/.bashrc && source $HOME/.bashrc"
-
-alias rhino="java -jar $HOME/Code/javascript/rhino1_7R2/js.jar"
-alias jslint="java -jar $HOME/Code/javascript/rhino1_7R2/js.jar $HOME/Code/javascript/jslint.js"
-alias yuicompressor="java -jar $HOME/Code/javascript/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar"
-alias closurecompiler="java -jar $HOME/Code/javascript/compiler-latest/compiler.jar"
 
 alias setgitignore="cp $HOME/Dropbox/configs/gitignore .gitignore"
 
 alias vmwarefusion_setports="sudo vim /Library/Application\ Support/VMware\ Fusion/vmnet8/nat.conf"
 alias vmwarefusion_reloadports="sudo /Library/Application\ Support/VMware\ Fusion/boot.sh --restart"
 
-alias jstd="$CODEHOME/scripts/shell/js_test_driver.sh"
-alias reinstall-janus="$CODEHOME/scripts/shell/reinstall_janus.sh"
-
-alias sync-sublime-session="$HOME/Dropbox/configs/sublimetext2/sync_sublime_session.sh"
-alias set-sublime-session="$HOME/Dropbox/configs/sublimetext2/set_sublime_session.sh"
-
 alias sync-music="rsync -avz /Users/trodrigues/Music/iTunes/iTunes\ Media/Music/* /Volumes/external/music/"
 
 alias git-firewall-bypass="git config --global url.\"https://\".insteadOf git://"
+
+docker-rm-ps(){
+  docker rm `docker ps -a -q`
+}
+docker-rmi-untagged(){
+  docker rmi `docker images | awk '$$2 ~ /none/ {print $$3}'`
+}
+
+docker-start(){
+  dinghy up
+  eval "$(docker-machine env dinghy)"
+}
+docker-stop-all(){
+  docker stop `docker ps -q`
+}
+docker-env-eval(){
+  eval "$(docker-machine env $1)"
+}
+
+if [ `docker-machine ls --filter name=dinghy --filter state=Running -q|wc -l` -gt 0 ] ; then
+  eval "$(docker-machine env dinghy)"
+fi
+
